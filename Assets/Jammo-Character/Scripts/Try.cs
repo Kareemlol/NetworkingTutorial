38a10634bb54fb5ec7c9888112ef7a386f62c343
Kareem Rashed <kareemrashed2018@gmail.com>
2020-10-16 22:43:40 +0300

    Initial unity project

---
+++ Assets/Jammo-Character/Scripts/Try.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Try : MonoBehaviour {
+    private Rigidbody rb;
+    public int baseSpeed;
+    public int sprint;
+    private int currentSpeed;
+ 
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update ()
+    {
+        //this makes the player go where the camera is pointing
+        transform.position = transform.position + Camera.main.transform.forward*currentSpeed*Time.deltaTime;
+ 
+        //this makes the player sprint when the shift key is pressed
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = baseSpeed + sprint;
+        }
+        else
+        {
+            currentSpeed = baseSpeed;
+        }
+    }
+}
